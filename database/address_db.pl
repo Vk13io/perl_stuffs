e80e22ba011dd644a8f289755adff4d59112414c
Vikas K <83337729+Vk13io@users.noreply.github.com>
2025-04-02 21:08:29 +0530

    Update address_db.pl

--- database/address_db.pl
+++ database/address_db.pl
@@ -1,26 +1,6 @@
 #!/usr/bin/perl
-#********************************************************************************************
-#Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
- 
-#All Rights Reserved.
-
-#This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-#It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-#courses or received any written authorization from Maven Silicon.
-
-
-#Webpage:  www.maven-silicon.com
-
-#Filename:	   address_db.perl   
-
 #Description:      Script to create an address database
 
-#Author Name:      Susmita Nayak
-
-#Version: 1.0
-#*********************************************************************************************
 
 print "\n \U Would you like to input any address to the data base \n";
 print "\n -------- y-agree n-disagree ---------- \n";
